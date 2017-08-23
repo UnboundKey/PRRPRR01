ba6c5412568f908cbc55f3ef6f4f8a3bcf687f68
sadman <godiset@gmail.com>
2017-08-23 19:28:29 +0200

    Hello world in C++

--- UPG0/C++/fuckmylife.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-string help = "Why is this so hard?";
-
-int main() {
-	cout << help;
-	cout << endl;
-}
--- UPG0/C++/libtest.cpp
+++ UPG0/C++/libtest.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "libs/test.cpp"
 
- basic_string<char> name;
+
 
 int main() {
 	cout << "String were needlessly complicated for me. until I realized that all you had to do was to have the std namespace. Otherwise you have to use basic_string<char> which is kind of hard to remember, don't you think?";
